279917142c3a44b05cc38642a5e0fcf74ef75a72
Geng Sun <gsun@iastate.edu>
2023-03-30 14:32:10 -0500

    Custom event binding with @output()

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,10 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  loadedFeature = 'recipes';
   title = 'shopping-list';
+
+  onNavigate(feature: string) {
+    this.loadedFeature = feature;
+  }
 }
