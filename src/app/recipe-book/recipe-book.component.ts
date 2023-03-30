00ec9c32103554e7b9a8f3039736ef61d59b74e6
Geng Sun <gsun@iastate.edu>
2023-03-30 16:07:48 -0500

    Passing data with event and  property binding.

--- src/app/recipe-book/recipe-book.component.ts
+++ src/app/recipe-book/recipe-book.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Recipe } from './recipe.model';
 
 @Component({
   selector: 'app-recipe-book',
@@ -6,5 +7,9 @@ import { Component } from '@angular/core';
   styleUrls: ['./recipe-book.component.css']
 })
 export class RecipeBookComponent {
+  selectedRecipe: Recipe;
 
+  onNavigate(recipe: Recipe) {
+    this.selectedRecipe = recipe;
+  }
 }
--- src/app/recipe-book/recipe-list/recipe-list.component.ts
+++ src/app/recipe-book/recipe-list/recipe-list.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Output } from '@angular/core';
 import { Recipe } from '../recipe.model';
 
 @Component({
@@ -7,11 +7,17 @@ import { Recipe } from '../recipe.model';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent {
+  @Output() recipeWasSelected = new EventEmitter<Recipe>();
   recipes: Recipe[] = [
     new Recipe('A Test Recipe', 'This is simply a test', 'https://upload.wikimedia.org/wikipedia/commons/1/15/Recipe_logo.jpeg'),
+    new Recipe('Zhao zhu', 'She is from pig planet', 'https://cdn.wikifarmer.com/wp-content/uploads/2022/02/%E5%85%B3%E4%BA%8E%E7%8C%AA%E7%9A%84%E5%B8%B8%E8%A7%81%E9%97%AE%E9%A2%98%E5%92%8C%E8%A7%A3%E7%AD%94.jpg'),
   ];
 
   onNewRecipe() {
-    
+
+  }
+
+  onRecipeSelected(recipe: Recipe) {
+    this.recipeWasSelected.emit(recipe);
   }
 }
