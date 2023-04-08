de9dd9377db5fd94283f9d1ff958b136a88122a3
Geng Sun <gsun@iastate.edu>
2023-04-08 00:29:50 -0500

    Added function to allow add ingredients to shopping list

--- src/app/shopping-list/shopping-list.component.ts
+++ src/app/shopping-list/shopping-list.component.ts
@@ -1,23 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { ingredient } from '../sheared/ingredient.model';
+import { ShoppingService } from '../services/shopping.service';
 
 @Component({
   selector: 'app-shopping-list',
   templateUrl: './shopping-list.component.html',
   styleUrls: ['./shopping-list.component.css']
 })
-export class ShoppingListComponent {
-  ingredients: Map<string, number> = new Map<string, number>([
-    ["apples", 5],
-    ["tomatoes", 10]
-  ]);
+export class ShoppingListComponent implements OnInit {
+  ingredients: Map<string, number> = new Map<string, number>();
 
-  onAddIngredient(ingredient: ingredient) {
-    let ingredientName: string = ingredient.name.toLowerCase();
-    if (!this.ingredients.has(ingredientName)) {
-      this.ingredients.set(ingredientName, ingredient.amount);
-    } else {
-      this.ingredients.set(ingredientName, +this.ingredients.get(ingredientName) + +ingredient.amount);
-    }
+  constructor(private shoppingService: ShoppingService) { }
+
+  ngOnInit(): void {
+    this.ingredients = this.shoppingService.getIngredients();
+    this.shoppingService.ingredientsChanged.subscribe((ingredients: Map<string, number>) => {
+      this.ingredients = ingredients;
+    });
   }
 }
